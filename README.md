fe969aeabebb0114e109dd5e07beb8eff588d296
Erik Aldahir Parra Santillán <pandiwix13@gmail.com>
2019-04-19 02:35:06 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+**Fusion33**
+
+Esta es un a pagina web en donde puedes colocar tu información profesional y ser presentada como tu CV.
+
+Link: https://fusion33iv.github.io/test-website-03/
+
+-Departamento Web de Fusion33.
---
+++ index.js
@@ -0,0 +1,18 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.sidenav');
+//   var instances = M.Sidenav.init(elems);
+// });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.collapsible');
+//   var instances = M.Collapsible.init(elems)
+// });
+
+// var instance = M.Tabs.init(el);
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.carousel');
+//   var instances = M.Carousel.init(elems);
+// });
+
+// var instance = M.Carousel.getInstance(elem);
